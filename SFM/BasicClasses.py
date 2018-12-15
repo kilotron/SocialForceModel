37173db359ea4d122bd1587b29625e3f79eb72c5
Elvis <cqycczq@126.com>
2018-12-15 10:57:35 +0800

    完善了函数的说明和场景文件的定义

--- SFM/BasicClasses.py
+++ SFM/BasicClasses.py
@@ -1,5 +1,28 @@
+import random
+import math
+
+"""
+    为了简化，我们对每个行人的参数A(N), B(m), desired_speed(m/s), mass(kg)取相同的值。
+    肩宽的一半radius(m)服从U(0.25, 0.35)，也就是肩宽在区间[0.5m, 0.7m]服从均匀分布。
+    特征时间ch_time是0.5s
+"""
+param = {
+    'A': 2000.0,
+    'B': 0.08,
+    'desired_speed': 1.0,
+    'mass': 80.0,
+    'r_upper': 0.35,
+    'r_lower': 0.25,
+    'ch_time': 0.5
+}
+
 
 class Vector2D:
+    """ 二维向量，表示力、速度、位置或者方向。
+    Attributes:
+        x: 横坐标
+        y: 纵坐标
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -7,59 +30,86 @@ class Vector2D:
     def __add__(self, other):
         return Vector2D(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return Vector2D(self.x - other.x, self.y - other.y)
+
     def __mul__(self, scalar):
         if isinstance(scalar, (int, float)):
             return Vector2D(self.x * scalar, self.y * scalar)
         else:
             return NotImplemented
 
+    def norm(self):
+        return math.sqrt(self.x ** 2, self.y ** 2)
+
     def __str__(self):
         return 'Vector2D(%.2f, %.2f)' % (self.x, self.y);
 
 
 class Circle:
-    '''
-    Circle表示行人，pos是位置坐标，vel是当前速度，mass是质量
-    '''
-    desired_velocity = 1
+    """ Circle表示行人
+    Attributes:
+        pos: 位置向量
+        vel: 当前速度
+        mass: 质量
+        radius: 圆的半径，或人肩宽的一半
+    """
 
     def __init__(self, x, y, vx, vy, mass):
         self.pos = Vector2D(x, y)
         self.vel = Vector2D(vx, vy)
-        self.radius = 0 # 改一下，这个是肩宽
+        self.radius = random.uniform(param['r_lower'], param['r_upper'])
         self.mass = mass
 
-    def get_ped_force(self, others):
+    def distance_to(self, other):
+        """ 计算与参数other的距离
+        根据other的类型（Circle，或墙或障碍物）分别计算
+        :param other:
+        :return:
         """
-        使用公式fij = Ai * e^((rij-dij)/Bi) * nij(公式第一项)计算和其他行人间的作用力
-        注意nij是一个单位向量,nij = (ri - rj) / dij，其中dij = ||ri - rj||
+        return 0.0
+
+    def ped_repulsive_force(self, others):
+        """ 计算行人与其他行人间的排斥力
+
+        使用公式:
+            f_i = ∑(j)f_ij 结果
+            f_ij = A * e^((r_ij - d_ij) / B) * n_ij
+            r_ij = r_i + r_j 半径之和
+            d_ij = ||r_i - r_j|| 圆心距离
+            n_ij = (r_i - r_j) / d_ij 单位方向向量
+
         :param others: 其他的行人，是一个Circle的列表
-        :return: 其他行人们对此人的合力
+        :return: 其他行人们对此人的合力f_i
         """
         return Vector2D(0, 0)
 
-    def get_wall_force(self, scene):
-        """
-        使用公式fiW = Ai * e^((ri-diW)/Bi) * niW(公式第一项)计算与墙的作用力
+    def wall_repulsive_force(self, scene):
+        """ 计算与障碍物或墙的排斥力
+
+        使用公式:
+            ∑(W)f_iW 结果
+            f_iW = A * e^((ri-diW)/B) * niW
         注意niW是一个向量,niW的方向是由墙指向行人
+
         :param scene: 场景，包括障碍物和墙
         :return: 所有墙和障碍物对此人的合力
         """
         return Vector2D(0, 0)
 
-    def get_desired_force(self):
-        """
-        计算期望力（自己编的名字），使用公式m *(v * e - vc) / t_c，m是质量，v是期望速度（上面那个变量），
-        e是调用A*的get_direction(scene, circles)获得的单位方向向量，即期望的方向，vc是当前速度，t_c是特征时间
-        取0.5s
+    def desired_force(self):
+        """ 计算期望力
+        使用公式:
+            m * (v * e - vc) / t_c
+            m是质量，v是期望速度，e是期望方向(get_direction())
+            vc是当前速度，t_c是特征时间
         :return: 期望力
         """
         return Vector2D(0, 0)
 
     def get_force(self, scene):
-        """
-        使用get_ped_force()和get_wall_force()和get_desired_force()计算合力
-        :param circle:
+        """ 计算合力
+        :param
         :return: 合力
         """
         return Vector2D(0, 0)
@@ -81,41 +131,42 @@ class Circle:
 
 
 class Box:
-    '''
-    Box表示障碍物或墙(矩形)，p1和p2是矩形对角线上的两个点的坐标。
-    '''
+    """ Box表示障碍物或墙(矩形)
+    Attributes:
+        p1:
+        p2: 矩形对角线上的两个点的坐标。
+    """
+
     def __init__(self, x1, y1, x2, y2):
         self.p1 = Vector2D(x1, y1)
         self.p2 = Vector2D(x2, y2)
 
 class Scene:
+    """ Scene是一个场景，包括静态的墙、障碍物和动态的行人
+    Attributes:
+        boxes: 障碍物和墙们，Box类型的列表
+        dests: 目标位置们，Box类型的列表，可以包含在boxes中
+        peds: 行人们，Circle类型，可以是一个列表
     """
-    Scene是一个场景，包括静态的墙、障碍物和动态的行人
-    boxes 是障碍物和墙们
-    target是目标位置
-    peds是行人们，Circle类型，可以是一个列表
-    """""
     def __init__(self):
-        self.target = None
+        # 修改这个方法来初始化场景
+        self.dests = None
         self.peds = None
         self.boxes = None
 
     def load(self, path):
-        """
-        从文件中加载场景, 文件的格式待设计
+        """ 从文件中加载场景
         :param path: 文件路径
         :return:
         """
 
     def update(self):
-        """
-        推进一个时间步长，更新行人们的位置
+        """ 推进一个时间步长，更新行人们的位置
         :return:
         """
 
     def save(self, path):
-        """
-        保存场景到路径path，只含有墙和障碍物，不含人
+        """ 保存场景到路径path
         :param path:
         :return:
         """
--- SFM/PathFinder.py
+++ SFM/PathFinder.py
@@ -10,9 +10,9 @@ class AStartPathFinder:
         self.scene = 1
 
     @staticmethod
-    def get_direction(scene, source, circles):
-        """
-        scene是Scene类型，source是行人（Circle类型），circles是所有的人（Circle列表）
+    def get_direction(scene, source):
+        """ 寻找路径，获得下一步运动的方向
+        scene是Scene类型，source是行人（Circle类型）
         :return: 返回期望方向e，类型为Vector2D，要求e是单位向量e.x^2 + e.y^2 = 1
         """
 
