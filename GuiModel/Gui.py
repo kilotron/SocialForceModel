ca7b51868b256dd0459adc2dfedbbbe30cbe164c
rrrral <295536022@qq.com>
2018-12-18 15:33:57 +0800

    允许从文件中读写模型

--- GuiModel/Gui.py
+++ GuiModel/Gui.py
@@ -8,10 +8,11 @@ import tkinter
 from SFM import BasicClasses
 
 color_list = []
-MAX_X = 300
-MAX_Y = 300
+MAX_X = 500
+MAX_Y = 500
 MAX_COLOR = 50
 CANVAS_BG = "white"
+default_path = "D://save.p"
 
 
 def color_list_init(num):
@@ -47,18 +48,38 @@ def move_box_right(gui):
 
 
 def move_box_left_btn(event, gui):
-    th = threading.Thread(target=move_box_left, args=(gui, ))
+    th = threading.Thread(target=move_box_left, args=(gui,))
     th.setDaemon(True)
     th.start()
 
 
 def move_box_right_btn(event, gui):
-    th = threading.Thread(target=move_box_right, args=(gui, ))
+    th = threading.Thread(target=move_box_right, args=(gui,))
     th.setDaemon(True)
     th.start()
 
 
-class SfmGuiTest:
+def begin_simulate(gui):
+    for i in range(1000):
+        time.sleep(0.5)
+        gui.scene.update()
+        # print(gui.scene.peds)
+        for ped in gui.peds:
+            x = ped[0].pos.x
+            y = ped[0].pos.y
+            # print(x, y)
+            r = 2  # ped[0].radius * 15
+            gui.canvas.coords(ped[1], (x - r, y - r, x + r, y + r))
+            # gui.canvas.coords(ped[1], ped[0].pos.x, ped[0].pos.y)
+
+
+def begin_simulate_btn(event, gui):
+    th = threading.Thread(target=begin_simulate, args=(gui,))
+    th.setDaemon(True)
+    th.start()
+
+
+class SfmGui:
     def __init__(self, scene):
         self.click_coordinate = {"x": -1, "y": -1, "box": None}
         self.boxes = []  # 障碍物
@@ -66,16 +87,19 @@ class SfmGuiTest:
         self.peds = []  # 当前所有人的位置，是一个列表
 
         self.default_scene = copy.deepcopy(scene)
+        self.scene = scene
         self.root = tkinter.Tk()
+        self.root.resizable(False, False)
         self.root.title("社会力模型模拟")
-        self.canvas = tkinter.Canvas(self.root, bg=CANVAS_BG, width=MAX_X+50, height=MAX_Y+50)
+        self.canvas = tkinter.Canvas(self.root, bg=CANVAS_BG, width=MAX_X + 50, height=MAX_Y + 50)
 
         self.canvas.pack(side=tkinter.LEFT)
         self.change_scene(scene)
 
-        # 实现界面右边的功能
+        # 实现界面右边的功能 D:\0bianchen\python\图形编程\save.p
         self.frame = tkinter.Frame(self.root)
         self.frame.pack(side=tkinter.RIGHT)
+        self.the_path = tkinter.Entry(self.frame, width=30)
         self.bind_btn()
 
     def get_click(self, event):
@@ -94,9 +118,20 @@ class SfmGuiTest:
                 self.click_coordinate['y'] = y_now
                 self.click_coordinate['box'] = box
                 return
-            self.click_coordinate['x'] = -1
-            self.click_coordinate['y'] = -1
-            self.click_coordinate['box'] = None
+        for box in self.dests:
+            x0 = box[0].p1.x
+            y0 = box[0].p1.y
+            x1 = box[0].p2.x
+            y1 = box[0].p2.y
+            if ((x_now < x0) ^ (x_now < x1)) & ((y_now < y0) ^ (y_now < y1)):
+                # print("true")
+                self.click_coordinate['x'] = x_now
+                self.click_coordinate['y'] = y_now
+                self.click_coordinate['box'] = box
+                return
+        self.click_coordinate['x'] = -1
+        self.click_coordinate['y'] = -1
+        self.click_coordinate['box'] = None
 
     def click_release(self, event):
         # 画布中鼠标释放时，执行此函数将相应的box移动
@@ -125,23 +160,44 @@ class SfmGuiTest:
     def change_scene(self, scene):
         # 改变场景
         self.canvas.delete(tkinter.ALL)
+        self.scene = scene
         self.boxes = [[x, -1] for x in scene.boxes]  # 障碍物
         self.dests = [[x, -1] for x in scene.dests]  # 目标位置
         self.peds = [[x, -1] for x in scene.peds]  # 当前所有人的位置，是一个列表
         self.init_canvas()
-
-    def save(self, event, path):
-        # 保存当前场景
+        '''
         boxes = [x[0] for x in self.boxes]
         dests = [x[0] for x in self.dests]
         peds = [x[0] for x in self.peds]
         scene = BasicClasses.Scene(boxes=boxes, peds=peds, dests=dests)
         scene.save(path)
+        '''
+
+    def save(self, event, path):  # 保存当前场景
+        input_path = self.the_path.get()
+        # print(input_path)
+        try:
+            f = open(input_path, "wb")
+            f.close()
+        except OSError:
+            self.scene.save(path)
+            return
+        self.scene.save(input_path)
 
     def load(self, event, path):
         # 读取场景
+        input_path = self.the_path.get()
         scene = BasicClasses.Scene()
-        scene.load(path)
+        # print(input_path)
+        try:
+            f = open(input_path, "rb")
+            f.close()
+        except OSError:
+            scene.load(path)
+            self.default_scene = copy.deepcopy(scene)
+            self.change_scene(scene)
+            return
+        scene.load(input_path)
         self.default_scene = copy.deepcopy(scene)
         self.change_scene(scene)
 
@@ -152,16 +208,16 @@ class SfmGuiTest:
         i = 0
         for box in self.boxes:
             # print(get_color(color_sum -1 - i))
-            self.add_box(box, fill=get_color(color_sum - 2 - i))
+            self.add_box(box, fill=get_color(color_sum - 1))
             i += 1
         i = 0
         for ped in self.peds:
             # print(get_color(i))
-            self.add_person(ped, fill=get_color(i))
+            self.add_person(ped, fill=get_color(i + 10))
             i += 1
         i = 0
         for dest in self.dests:
-            self.add_dest(dest, fill=get_color(color_sum-1))
+            self.add_dest(dest, fill=get_color(0))
             i += 1
 
     def bind_btn(self):
@@ -169,24 +225,32 @@ class SfmGuiTest:
         self.canvas.bind("<Button-1>", lambda x: self.get_click(x))
         self.canvas.bind("<ButtonRelease-1>", lambda x: self.click_release(x))
 
+        tkinter.Label(self.frame, text='保存及加载路径\n出错或不填,默认为\n'+default_path).pack()
+        # self.the_path = tkinter.Entry(self.frame)
+        self.the_path.pack()
+
+        new_button = tkinter.Button(self.frame, text="开始仿真")
+        new_button.bind("<Button-1>", lambda x: begin_simulate_btn(x, self))
+        new_button.pack()
+
         new_button = tkinter.Button(self.frame, text="让所有箱子左移")
         new_button.bind("<Button-1>", lambda x: move_box_left_btn(x, self))
-        new_button.pack()
+        # new_button.pack()
 
         new_button = tkinter.Button(self.frame, text="让所有箱子右移")
         new_button.bind("<Button-1>", lambda x: move_box_right_btn(x, self))
-        new_button.pack()
+        # new_button.pack()
 
         new_button = tkinter.Button(self.frame, text="重置场景")
         new_button.bind("<Button-1>", self.reset_scene)
         new_button.pack()
 
         new_button = tkinter.Button(self.frame, text="保存场景")
-        new_button.bind("<Button-1>", lambda x: self.save(x, "D://save.p"))
+        new_button.bind("<Button-1>", lambda x: self.save(x, default_path))
         new_button.pack()
 
         new_button = tkinter.Button(self.frame, text="加载场景")
-        new_button.bind("<Button-1>", lambda x: self.load(x, "D://save.p"))
+        new_button.bind("<Button-1>", lambda x: self.load(x, default_path))
         new_button.pack()
 
     def add_box(self, box, fill="black"):
@@ -203,8 +267,8 @@ class SfmGuiTest:
         # print(self.boxes.index(box))
         x = ped[0].pos.x
         y = ped[0].pos.y
-        r = ped[0].radius * 15
-        ped[1] = self.canvas.create_oval((x-r, y-r, x+r, y+r), fill=fill)
+        r = 2  # ped[0].radius * 15
+        ped[1] = self.canvas.create_oval((x - r, y - r, x + r, y + r), fill=fill)
 
     def add_dest(self, dest, fill="black"):
         x0 = dest[0].p1.x
@@ -231,14 +295,18 @@ if __name__ == '__main__':
     boxes = []
     dests = []
     peds = []
-    boxes.append(BasicClasses.Box(5, 5, 250, 10))
-    boxes.append(BasicClasses.Box(180, 40, 193, 160))
-    boxes.append(BasicClasses.Box(5, 200, 250, 215))
+    boxes.append(BasicClasses.Box(5, 0, 460, 10))
+    boxes.append(BasicClasses.Box(5, 450, 460, 460))
+    boxes.append(BasicClasses.Box(370, 40, 380, 420))
+
+    boxes.append(BasicClasses.Box(450, 0, 460, 70))
+    boxes.append(BasicClasses.Box(450, 120, 460, 450))
 
     peds.append(BasicClasses.Circle(30, 30, 3, 3, 1))
+    peds.append(BasicClasses.Circle(40, 50, 3, 3, 1))
 
-    dests.append(BasicClasses.Box(230, 30, 240, 160))
+    dests.append(BasicClasses.Box(450, 70, 460, 120))
     new_scene = BasicClasses.Scene(dests=dests, peds=peds, boxes=boxes)
-    g_gui = SfmGuiTest(new_scene)
+    g_gui = SfmGui(new_scene)
 
     g_gui.root.mainloop()
